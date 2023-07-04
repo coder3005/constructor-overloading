d715a397cc8511b3f1ccfe664079dcf472e38d4c
coder3005 <138576028+coder3005@users.noreply.github.com>
2023-07-04 15:06:10 +0530

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,33 @@
+/* Constructor Overloading */
+class A 
+{
+    int a; double b; String c;
+    A()
+    {
+        a=100; b=45.32; c="abhi";
+    }
+    A(int x)
+    {
+        a=x;
+
+    }
+    A(double y, String z)
+    {
+        b=y; c=z;
+
+    }
+}
+class B
+{
+    public static void main(String[] args) {
+        
+    A r=new A();
+    A r2=new A(10);
+    A r3=new A(23.89, "abhi");
+    System.out.println(r.a+" "+r.b+" "+r.c);
+    System.out.println(r2.a);
+    System.out.println(r3.b+" "+r3.c);
+    }
+    
+    
+}
